997b2213c351c27030e099dd327078a20e8998d1
Revi <s1492e034@gmail.com>
2018-06-02 10:24:46 +0800

    add friend button to user index & validate uniqueness of friend_id & add create action for friendship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,6 @@
 class Friendship < ApplicationRecord
+  validates :friend_id, uniqueness: { scope: :user_id } #唯一性驗證
+
   belongs_to :user #每筆friendship記錄屬於加他人好友的user
   belongs_to :friend, class_name: "User" #每筆friendship紀錄屬於被加為好友的user
 end
