fb15146937c96fa3580f16ad9f15ff6e0645eec4
Revi <s1492e034@gmail.com>
2018-06-06 16:00:48 +0800

    validate uniqueness of friend_id in user_id scope in friend_request model

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,4 +1,7 @@
 class FriendRequest < ApplicationRecord
+  validates :friend_id, uniqueness: { scope: :user_id }
+  #唯一性驗證,user不能重複送出friend_request給同一user
+
   belongs_to :user #每筆friend_request記錄,屬於送出friend_request的user
   belongs_to :friend, class_name: "User" #每筆friend_request記錄,屬於收到friend_request的user
 end
