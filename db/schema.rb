4f6f7186922e846308c15bf10d36eb9fea57bae1
Revi <s1492e034@gmail.com>
2018-06-06 15:18:02 +0800

    create friend_request model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_02_015019) do
+ActiveRecord::Schema.define(version: 2018_06_06_071543) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2018_06_02_015019) do
   create_table "followships", force: :cascade do |t|
     t.integer "user_id"
     t.integer "following_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "friend_requests", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
