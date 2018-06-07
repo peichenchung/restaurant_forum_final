6584a7d915e60d794ea061cc4b5a81f41b74d567
Revi <s1492e034@gmail.com>
2018-06-07 10:22:24 +0800

    add friend_request#update action for accepting friend_request and create mutual friendships

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,4 +1,5 @@
 class FriendRequestsController < ApplicationController
+
   def create #送出交友請求
     friend = User.find(params[:friend_id])
     #使用者在User列表上點下某User的Friend按鈕,網址會攜帶參數friend_id=user.id
@@ -14,6 +15,21 @@ class FriendRequestsController < ApplicationController
     end
   end
 
+
+  def update #接受交友請求
+    #雙方成為好友(在Friendship新增雙方的friendships記錄)
+    current_user.friendships.create!(friend_id: params[:friend_id])
+    current_user.inverse_friendships.create!(user_id: params[:friend_id])
+
+    #刪除(收到的)交友請求記錄
+    @friend_request = current_user.inverse_friend_requests.where(friend_id: params[:id]).first
+    @friend_request.destroy
+
+    flash[:notice] = "You just accepted a friend request"
+    redirect_back(fallback_location: root_path)
+  end
+
+
   def destroy #取消送出的交友請求 #cancel按鈕放在自己的好友列表中
     @friend_request = current_user.friend_requests.where(friend_id: params[:friend_id]).first
 
@@ -24,4 +40,5 @@ class FriendRequestsController < ApplicationController
     flash[:alert] = "You just canceled a friend request"
     redirect_back(fallback_location: root_path)
   end
+  
 end
